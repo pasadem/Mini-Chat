e887d2494aa04831cbf22b99795ab264a60e1074
pasadem <pasadem@ukr.net>
2024-07-17 16:57:31 +0300

    latest

--- src/components/toastParams.js
+++
@@ -1,9 +0,0 @@
-export default {
-    position: 'top-right',
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-  };
--- src/contexts/index.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-
-const AuthContext = createContext({});
-const ApiContext = createContext({});
-
-export { AuthContext, ApiContext };
